72fe57128dd1d01a34ab49d2717df72cf13f45fd
MdTarikulIslam <nayon1820@gmail.com>
2023-06-22 16:46:27 +0600

    22 june, 2022

--- src/components/ConsultingPage/Consalts/AI/AI.js
+++ src/components/ConsultingPage/Consalts/AI/AI.js
@@ -13,41 +13,41 @@ const AI = () => {
         </div>
         <div>
           <CardBody className="card-body keep-scrolling py-0">
-            <Typography className=" text-lg text-center lg:text-justify lg:text-2xl sticky top-0 bg-white mt-10  md:mt-0 font-bold text-blue-600">
+            <Typography className=" text-lg text-center lg:text-left lg:text-2xl sticky top-0 bg-white mt-10  md:mt-0 font-bold text-blue-600">
               The Age of Technology Has Arrived: AI and Machine Learning Are the
               Future
             </Typography>
             {allData.map((data) => {
               return (
                 <div>
-                  <Typography className="my-5 text-md text-justify lg:text-xl font-bold text-blue-900">
+                  <Typography className="my-5 text-md text-left lg:text-xl font-bold text-blue-900">
                     {data.title}
                   </Typography>
-                  <Typography className=" text-sm text-justify font-regular lg:text-lg text-black">
+                  <Typography className=" text-sm text-left font-regular lg:text-lg text-black">
                     {data.desc}
                   </Typography>
-                  <Typography className="my-5 text-md text-justify lg:text-xl font-bold text-green-900">
+                  <Typography className="my-5 text-md text-left lg:text-xl font-bold text-green-900">
                     {data.title2}
                   </Typography>
-                  <Typography className=" text-sm text-justify font-regular lg:text-lg text-black">
+                  <Typography className=" text-sm text-left font-regular lg:text-lg text-black">
                     {data.desc2}
                   </Typography>
-                  <Typography className="my-5 text-md text-justify lg:text-xl font-bold text-green-900">
+                  <Typography className="my-5 text-md text-left lg:text-xl font-bold text-green-900">
                     {data.title3}
                   </Typography>
-                  <Typography className=" text-sm text-justify font-regular lg:text-lg text-black">
+                  <Typography className=" text-sm text-left font-regular lg:text-lg text-black">
                     {data.desc3}
                   </Typography>
-                  <Typography className="my-5 text-md text-justify lg:text-xl font-bold text-green-900">
+                  <Typography className="my-5 text-md text-left lg:text-xl font-bold text-green-900">
                     {data.title4}
                   </Typography>
-                  <Typography className=" text-sm text-justify font-regular lg:text-lg text-black">
+                  <Typography className=" text-sm text-left font-regular lg:text-lg text-black">
                     {data.desc4}
                   </Typography>
-                  <Typography className="my-5 text-md text-justify lg:text-xl font-bold text-green-900">
+                  <Typography className="my-5 text-md text-left lg:text-xl font-bold text-green-900">
                     {data.title5}
                   </Typography>
-                  <Typography className=" text-sm text-justify font-regular lg:text-lg text-black">
+                  <Typography className=" text-sm text-left font-regular lg:text-lg text-black">
                     {data.desc5}
                   </Typography>
                 
